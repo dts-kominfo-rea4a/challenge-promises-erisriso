fe4c991451a36ed1fd5bd68847f617c22a9a069d
Eris Riso <erisriso@gmail.com>
2022-09-18 23:39:38 +0700

    Tambah fungsi promiseOutput

--- promise.js
+++ promise.js
@@ -1,8 +1,35 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+
+
+const promiseOutput = (isiParam) => {
+  let jumlah = 0;
+  return new Promise(async (resolve,rejected) => {
+    let x = await  promiseTheaterIXX()
+        .then((data) => {
+          data.forEach(element => {
+            if(element.hasil===isiParam){
+              jumlah++;
+            }
+          });
+      });
+      
+    let y = await  promiseTheaterVGC()
+        .then((data) => {
+          data.forEach(element => {
+            if(element.hasil===isiParam){
+              jumlah++;
+            }
+          });
+      });
+
+    resolve(jumlah)
+    
+  });
+};
 
 module.exports = {
   promiseOutput,
 };
+
